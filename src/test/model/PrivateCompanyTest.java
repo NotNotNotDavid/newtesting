63cea966cf31746a0e04890716db48503e24aeba
shuan103 <shuan103@student.ubc.ca>
2024-10-09 13:10:31 -0700

    Added tests for PrivateCompany class

---
+++ src/test/model/PrivateCompanyTest.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrivateCompanyTest{
+    
+    private PrivateCompany testPrivateCompany;
+
+    @BeforeEach
+    public void runBefore(){
+        testPrivateCompany = new PrivateCompany("Dogo Railway", 50, true);
+    }
+
+    @Test
+    public void testConstructor(){
+        assertEquals("Dogo Railway", testPrivateCompany.getName());
+        assertEquals(50, testPrivateCompany.getPrice());
+        assertEquals(false, testPrivateCompany.isBought());
+    }
+
+}
