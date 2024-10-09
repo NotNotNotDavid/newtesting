443c980c291915f588ebec03a59d6e774ffd6a10
shuan103 <shuan103@student.ubc.ca>
2024-10-09 13:06:35 -0700

    added specification for PrivateCompany class

---
+++ src/main/model/PrivateCompany.java
@@ -0,0 +1,31 @@
+package model;
+
+public class PrivateCompany {
+
+    /*
+    * REQUIRES:
+    * conpanyName has a non-zero length;
+    * 10 >= sharesLeft >= 0.
+    *
+    * EFFECTS:
+    * name on public company is set to companyName;
+    * price of the public company is set to price;
+    * amount of shares bought is set to sharesBought;
+    * amount of shares left is set to sharesLeft;
+    */
+    public PrivateCompany(String companyName, int price, boolean isBought){
+    }
+
+    public String getName(){
+        return "";
+    }
+
+    public int getPrice(){
+        return 0;
+    }
+
+    public boolean isBought(){
+        return false;
+    }
+
+}
