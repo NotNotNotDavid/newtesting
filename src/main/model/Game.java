4133c89fc63924d4870862bf48abc010b6c640ef
shuan103 <shuan103@student.ubc.ca>
2024-10-10 20:36:42 -0700

    Created specifications for Game class

---
+++ src/main/model/Game.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.ArrayList;
+
+
+public class Game {
+    
+    //EFFECTS: creates a new game with lists of players, public companies and private companies.
+    public Game(){
+        //stub
+    }
+
+    public ArrayList<PublicCompany> getListofPublicCompanies(){
+        return null; //stub
+    }
+
+    public ArrayList<PrivateCompany> getListofPrivateCompanies(){
+        return null; //stub
+    }
+
+    public ArrayList<Player> getListOfPlayers(){
+        return null; //stub
+    }
+
+
+    
+}
