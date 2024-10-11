9e04d1ec5d0e1ff92deb3e911f8ac94662d6e7bc
shuan103 <shuan103@student.ubc.ca>
2024-10-11 13:02:09 -0700

    Fixed code coverage for PublicCompany

--- src/test/model/PublicCompanyTest.java
+++ src/test/model/PublicCompanyTest.java
@@ -21,4 +21,47 @@ public class PublicCompanyTest {
         assertEquals(10, testPublicCompany.getSharesLeft());
     }
 
+    @Test 
+    public void testEquals(){
+        assertTrue(testPublicCompany.equals(testPublicCompany));
+    }
+
+    @Test 
+    public void testNotEqualsName(){
+        PublicCompany testCopyPublicCompany = new PublicCompany("Dogo Railway", 100, 10);
+        assertFalse(testPublicCompany.equals(testCopyPublicCompany));
+    }
+
+    @Test 
+    public void testNotEqualsPrice(){
+        PublicCompany testCopyPublicCompany = new PublicCompany("Tosa Railway", 200, 10);
+        assertFalse(testPublicCompany.equals(testCopyPublicCompany));
+    }
+
+    @Test 
+    public void testNotEqualsShares(){
+        PublicCompany testCopyPublicCompany = new PublicCompany("Tosa Railway", 100, 9);
+        assertFalse(testPublicCompany.equals(testCopyPublicCompany));
+    }
+
+    @Test 
+    public void testNotObjectEquals(){
+        assertFalse(testPublicCompany.equals(5));
+    }
+
+    @Test 
+    public void testNullNotEquals(){
+        assertFalse(testPublicCompany.equals(null));
+    }
+
+    @Test 
+    public void testThisNotEquals(){
+        assertTrue(testPublicCompany.equals(testPublicCompany));
+    }
+
+    @Test 
+    public void testToString(){
+        assertEquals("Public Company [" + testPublicCompany.getName() + "]", testPublicCompany.toString());
+    }
+
 }
