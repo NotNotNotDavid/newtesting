041017506853274aed6b025d31a3b50d4e2e77db
shuan103 <shuan103@student.ubc.ca>
2024-10-11 12:49:09 -0700

    Completed code coverage for PrivateCompany class

--- src/test/model/PrivateCompanyTest.java
+++ src/test/model/PrivateCompanyTest.java
@@ -12,6 +12,7 @@ public class PrivateCompanyTest {
     @BeforeEach
     public void runBefore() {
         testPrivateCompany = new PrivateCompany("Dogo Railway", 50, true);
+
     }
 
     @Test
@@ -19,6 +20,52 @@ public class PrivateCompanyTest {
         assertEquals("Dogo Railway", testPrivateCompany.getName());
         assertEquals(50, testPrivateCompany.getPrice());
         assertEquals(true, testPrivateCompany.isBought());
+    }
+
+    @Test 
+    public void testEquals(){
+        PrivateCompany testCopyPrivateCompany = new PrivateCompany("Dogo Railway", 50, true);
+        assertTrue(testPrivateCompany.equals(testCopyPrivateCompany));
+    }
+
+    @Test 
+    public void testNotEqualsName(){
+        PrivateCompany testNotCopyPrivateCompany = new PrivateCompany("Toya Railway", 50, true);
+        assertFalse(testPrivateCompany.equals(testNotCopyPrivateCompany));
+    }
+
+    @Test 
+    public void testNotEqualsPrice(){
+        PrivateCompany testNotCopyPrivateCompany = new PrivateCompany("Dogo Railway", 100, true);
+        assertFalse(testPrivateCompany.equals(testNotCopyPrivateCompany));
+    }
+
+    @Test 
+    public void testNotEqualsIsBought(){
+        PrivateCompany testNotCopyPrivateCompany = new PrivateCompany("Dogo Railway", 50, false);
+        assertFalse(testPrivateCompany.equals(testNotCopyPrivateCompany));
+    }
+
+
+    @Test 
+    public void testNotNotEquals(){
+        assertFalse(testPrivateCompany.equals(5));
+    }
+
+    @Test 
+    public void testNullNotEquals(){
+        assertFalse(testPrivateCompany.equals(null));
+    }
+
+    @Test 
+    public void testThisNotEquals(){
+        assertTrue(testPrivateCompany.equals(testPrivateCompany));
+    }
+
+
+    @Test 
+    public void testToString(){
+        assertEquals("Private Company [" + testPrivateCompany.getName() + "]", testPrivateCompany.toString());
     }
 
 }
