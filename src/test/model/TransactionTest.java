014a64ef297726bded4da186a7d4abc6f5e7736b
shuan103 <shuan103@student.ubc.ca>
2024-10-11 13:19:46 -0700

    Fixed coverage for TransactionTest

--- src/test/model/TransactionTest.java
+++ src/test/model/TransactionTest.java
@@ -21,4 +21,51 @@ public class TransactionTest {
         assertEquals("Dogo Rail", testTransaction.getCompanyName());
         assertEquals("buy", testTransaction.getAction());
     }
+
+    @Test
+    public void testEquals(){
+        assertTrue(testTransaction.equals(testTransaction));
+    }
+
+    @Test
+    public void testNotEqualsPlayerName(){
+        Transactions transactionBad = new Transactions("Bobby", 100, "Dogo Rail", "buy");
+        assertFalse(testTransaction.equals(transactionBad));
+    }
+
+    @Test
+    public void testNotEqualsPrice(){
+        Transactions transactionBad = new Transactions("Jeff", 40, "Dogo Rail", "buy");
+        assertFalse(testTransaction.equals(transactionBad));
+    }
+
+    @Test
+    public void testNotEqualsCompanyName(){
+        Transactions transactionBad = new Transactions("Jeff", 100, "Iya Rail", "buy");
+        assertFalse(testTransaction.equals(transactionBad));
+    }
+
+    @Test
+    public void testNotEqualsAction(){
+        Transactions transactionBad = new Transactions("Jeff", 100, "Dogo Rail", "sell");
+        assertFalse(testTransaction.equals(transactionBad));
+    }
+
+    @Test
+    public void testNotEqualOther(){
+        assertFalse(testTransaction.equals(99));
+    }
+
+    @Test
+    public void testNotEqualNull(){
+        assertFalse(testTransaction.equals(null));
+    }
+
+
+    @Test
+    public void testToString(){
+        assertEquals("Transaction[" + "Player: " + testTransaction.getPlayerName()
+        + ", Price: " + testTransaction.getPrice() + ", Company: " + 
+        testTransaction.getCompanyName() + ", Action: " + testTransaction.getAction() + "]", testTransaction.toString());
+    }
 }
