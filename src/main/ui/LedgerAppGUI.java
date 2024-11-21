15c7ce059584e48f12ae383a3939a6b1429aaa85
shuan103 <shuan103@student.ubc.ca>
2024-11-21 14:11:58 -0800

    preliminary specifications for GUI

---
+++ src/main/ui/LedgerAppGUI.java
@@ -0,0 +1,25 @@
+package ui;
+
+public class LedgerAppGUI {
+
+    public static void main(String[] args) {
+        new LedgerAppGUI();
+    }
+
+    // EFFECTS: Creates a new Ledger with GUI
+    public LedgerAppGUI() {
+
+    }
+
+    // EFFECTS: starts the ui process
+    private void initializeUI() {
+    
+    }
+
+    // EFFECTS: displays the avaliable player options
+    private void displayPlayerOptions() { 
+
+    }
+
+}
+
